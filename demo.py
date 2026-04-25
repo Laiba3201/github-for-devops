84f0588f9c33c2c6290bf6a8465894b0609dd7a0
Laiba Hassan <munirlaiba954@gmail.com>
2026-04-26 03:24:37 +0500

    added text.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+def ash():
+    a=9,
+    b=9
+
+ash()
+
+result = dpi
+
+
---
+++ text.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    return a + b
+
+
+def main():
+    num1 = 5
+    num2 = 10
+    result = add_numbers(num1, num2)
+    print("Sum is:", result)
+
+
+if __name__ == "__main__":
+    main()
